d4e2e6ee286cdd8f1ae34f75f82af35f2d58316a
C5141506 <darshna.kumari@conduent.com>
2023-01-05 21:58:15 +0530

    dir changes

--- deepLinkingApp2/app/src/main/java/com/my/deeplinkingapp2/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.my.deeplinkingapp2
-
-object Constants {
-    const val DEEP_LINKING_DESTINATION_ACTION = "com.my.deeplinkingapp2.MainActivity"
-    const val DEEP_LINKING_DATA_TYPE = "text/plain"
-    const val PARAMETER1_NAME = "PARAMETER_1"
-    const val PARAMETER2_NAME = "PARAMETER_2"
-}
